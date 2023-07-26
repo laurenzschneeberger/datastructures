149a0317e486fc44f9d9d0e9dde45e17f25c1243
Laurenz Schneeberger <laurenzschneeberger@Laurenzs-MacBook-Pro.local>
2023-07-26 19:50:42 +0200

    finished up the functions

--- lists.py
+++ lists.py
@@ -18,6 +18,36 @@ class LinkedList:
         node = Node(data, self.head) 
         #sets the list's head to point to the current node
         self.head = node 
+    
+    def insert_at_end(self, data): 
+        # exception if llist is empty
+        if self.head is None: 
+            self.head = Node(data, None)
+            return
+        # else
+        itr = self.head
+        while itr.next:
+            itr = itr.next
+
+        itr.next = Node(data, None)
+
+    def remove_at(self, index): 
+        # scenario 1: index isn't valid
+        if index<0 or index>=self.get_length():
+            raise Exception("invalid index")
+        # scenario 2: index = 0, i.e. remove head
+        if index==0: 
+            self.head = self.head.next
+            return
+        # scenario 3: 
+        count = 0
+        itr = self.head 
+        while itr: 
+            if count == index-1: 
+                itr.next = itr.next.next 
+                break
+            itr = itr.next
+            count += 1
 
     def print(self):
         if self.head is None: #if the head doesn't tell us where to go, our list is empty
@@ -32,10 +62,47 @@ class LinkedList:
             itr = itr.next
         print(llstr)
 
+    def insert_values(self, data_list): 
+        self.head=None #??
+        for i in data_list: 
+            self.insert_at_end(i)
+
+    def get_length(self): 
+        count = 0 
+        itr = self.head
+        while itr:
+            count+=1
+            itr = itr.next
+        return count
+    
+    def insert_at(self, index, data): 
+        # sc 1: invalid index
+        if index<0 or index>self.get_length():
+            raise Exception("invalid index")
+        
+        #sc 2: insert at index 0 
+        if index==0: 
+            self.insert_at_beginning(data)
+
+        # sc 3: other indices
+        count = 0
+        itr = self.head
+        while itr:
+            if count == index-1: 
+                node = Node(data, itr.next)
+                itr.next = node
+                break
+            itr = itr.next
+
 if __name__ == '__main__':
     ll = LinkedList()
     ll.insert_at_beginning(5)
     ll.insert_at_beginning(89)
     ll.insert_at_beginning(9)
+    ll.insert_at_end(42)
+    ll.insert_values([1, 2, 3])
     ll.print()
+    ll.remove_at(0)
+    ll.insert_at(1, "lol")
+    ll.print()    
     
